3e7a4de52b710a9b7ee76e8f67a3b6aaed0a6096
Oscar Sánchez <65551928+Oscar-SanchezG@users.noreply.github.com>
2025-06-16 14:53:07 -0600

    Update main.py
    enmascarado

--- EEDME/main.py
+++ EEDME/main.py
@@ -8,11 +8,11 @@ Created on Wed May 17 05:43:11 2023
 import requests
 import json
 
-URL="https://www.ecodeli.mx/ApiRest/ApiEcodeliComercial/v2/PaginaEcodeli/get_Proveedores"
+URL="https://www.xxxxxxxxxxx.mx/ApiRest/Apixxxxx/v2/xxxxxxxxxx/get_Proveedores"
 #params={'0':{'strAccion':['1']}, 'body':'hihi', 'userId':1}
 payload={
 0: {'strAccion' : ['1']},
-1: {'strUsuario' : ['rnazario']},
+1: {'strUsuario' : ['xxxxxxxxxx']},
 2: {'strSucursal' : ['']},
 3: {'strPeriodo' : ['5']},
 4: {'strEjercicio' : ['2013']},
