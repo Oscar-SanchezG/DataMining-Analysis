f1f4e472169f458c94e2f9a3106f0c2372da30d4
Oscar Sánchez <65551928+Oscar-SanchezG@users.noreply.github.com>
2025-06-16 14:53:40 -0600

    Update prueba.py
    enmascarado

--- EEDME/prueba.py
+++ EEDME/prueba.py
@@ -7,7 +7,7 @@ Created on Wed May 17 07:26:57 2023
 import json
 d={
 0: {'strAccion' : ['1']},
-1: {'strUsuario' : ['rnazario']},
+1: {'strUsuario' : ['xxxxxxxxxx']},
 2: {'strSucursal' : ['']},
 3: {'strPeriodo' : ['5']},
 4: {'strEjercicio' : ['2013']},
